33d64a3f29fb9a4422c689a3f168ecbcc53c3a88
Burim Saliji <buz.saliji@gmail.com>
2023-09-19 20:41:46 +1000

    feat: add fetch for api sprite

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -10,11 +10,21 @@ export default class Pokemon extends React.Component {
 		}
 	}
 
+    async componentDidMount(){
+        let randomPokemonNUmber = Math.floor(Math.random() * 151) + 1;
+        let apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonNUmber}`);
+        let apiData = await apiResponse.json();
+
+        this.setState({pokemonData: apiData});
+    }
+
 	render(){
 		if (this.state.pokemonData.name){
 			return(
 				<div>
-					<h1>Pokemon data found!</h1>
+					<h1>{this.state.pokemonData.name}</h1>
+                    <img src={this.state.pokemonData.sprites.front_default} alt="Pokemon API sprite"/>
+
 				</div>
 			)
 		} else {
