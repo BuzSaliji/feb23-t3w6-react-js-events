af96aa0945e23c2a95ac4fd85fe0739bb9396ff9
Burim Saliji <buz.saliji@gmail.com>
2023-09-19 19:57:10 +1000

    feat: Pokemon component init

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-
 import React from 'react';
 import './App.css';
+import Pokemon from './Pokemon';
 
-// Page that lets us increase or decrease 
-// how many pokemon are on the screen
-// - keep track of how many pokemon to show
-// - iterate over a single, D.R.Y modular Pokemon component
+// Page that lets us increase or decrease
+// how many Pokemon are on screen 
+// App component needs to:
+// - keep track of how many Pokemon to show 
+// - iterate over a single, D.R.Y modular Pokemon component 
 // - button to increase Pokemon count
-// - button to decrease Pokemon count
+// - button to decrease Pokemon count 
 
 
 class App extends React.Component {
-  constructor () {
+  constructor(){
     super();
 
-    // start with at least 1 pokemon on screen
+    // start with at least ONE pokemon on screen 
     this.state = {
       pokemonCount: 1
     }
   }
 
-  render() {
+  render(){
     return(
-    <div>
-      <h1>Pokemon page</h1>
-    </div>
+      <div>
+        <h1>Pokemon page</h1>
+
+        <Pokemon></Pokemon>
+      </div>
     )
   }
 
